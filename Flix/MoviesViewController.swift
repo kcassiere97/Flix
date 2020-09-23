0f9b5a437a369096a9713e15a9217020d3f88e67
Mass Kizar Cassiere <kcassiere97@csu.fullerton.edu>
2020-09-22 23:42:35 -0700

    testing git

--- Flix/MoviesViewController.swift
+++ Flix/MoviesViewController.swift
@@ -45,7 +45,6 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
               // TODO: Get the array of movies
               // TODO: Store the movies in a property to use elsewhere
               // TODO: Reload your table view data
-
            }
         }
         task.resume()
